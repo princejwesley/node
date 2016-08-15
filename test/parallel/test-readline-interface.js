fc11a910bfa2029ebc28e76a53486d0bee874436
Prince J Wesley <princejohnwesley@gmail.com>
2016-08-15 17:37:01 +0530

    readline: key interval delay for \r & \n
    Emit two line events when there is a delay between
    CR('\r') and LF('\n')

--- test/parallel/test-readline-interface.js
+++ test/parallel/test-readline-interface.js
@@ -199,6 +199,26 @@ function isWarned(emitter) {
   assert.equal(callCount, expectedLines.length);
   rli.close();
 
+  // Delay between \r and \n should emit two line events
+  (() => {
+    const fi = new FakeInput();
+    const rli = new readline.Interface({
+      input: fi,
+      output: fi,
+      terminal: terminal
+    });
+    let callCount = 0;
+    rli.on('line', function(line) {
+      callCount++;
+    });
+    fi.emit('data', '\r');
+    setTimeout(() => {
+      fi.emit('data', '\n');
+      assert.equal(callCount, 2);
+      rli.close();
+    }, 200);
+  })();
+
   // \t when there is no completer function should behave like an ordinary
   //   character
   fi = new FakeInput();
